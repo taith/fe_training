3d175c583d87f0e6ea4a5e10ee19ddbfc409d193
taith <taisvbk@gmail.com>
2015-10-12 16:35:05 +0000

    user-logs

---
+++ db/migrate/20151012161906_create_user_logs.rb
@@ -0,0 +1,11 @@
+class CreateUserLogs < ActiveRecord::Migration
+  def change
+    create_table :user_logs do |t|
+      t.integer :user_id
+      t.string :log_data
+      t.datetime :time
+
+      t.timestamps null: false
+    end
+  end
+end
